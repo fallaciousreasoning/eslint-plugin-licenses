f3761c0774caac88de98b200e96accc937a6db01
Jay Harris <jay.harris@outlook.co.nz>
2022-11-04 17:18:27 +1300

    Add test for multi line inputs

--- tests/lib/rules/header.js
+++ tests/lib/rules/header.js
@@ -73,6 +73,23 @@ var react = require('react');
       }]
     },
     {
+      name: "Multi line string as input",
+      code: `
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+var react = require('react');
+      `,
+      options: [{
+        comment: 'line',
+        header: `Copyright (c) {YEAR} The Brave Authors. All rights reserved.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this file,
+you can obtain one at http://mozilla.org/MPL/2.0/.`
+      }]
+    },
+    {
       name: "Mult line header with year is valid (block comments)",
       code: `
 /*
